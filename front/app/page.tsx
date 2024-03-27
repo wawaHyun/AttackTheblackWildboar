50308b7839b923600ec2fd81b10f8943781f23b3
wawaHyun <sjrkchdkdy@gmail.com>
2024-03-27 21:57:20 +0900

    240327 pig front add

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,7 +1,9 @@
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { useState } from "react";
 
+const SERVER = 'http://localhost:8080'
 export default function Home() {
   // const [username, setUsername] = useState('')
   // const [password, setPassword] = useState('')
@@ -12,30 +14,68 @@ export default function Home() {
   // const handlePassword = (e: any) => {
   //   setPassword(e.target.value)
   // }
-  // alert(username+" "+password)
 
+  // const handleSumit = () =>{
+  //   const url = `${SERVER}/api/login`
+  //   const data = {username,password}
+  //   const config = {
+  //     headers: {
+  //         "Cache-Control": "no-cache",
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer blah ~`,
+  //         "Access-Control-Allow-Origin": "*",
+  //     }
+  // }
+  //   axios.post(url, data, config)
+  //   .then(res=>res.data)
+  // }
 
-  return (
-    <div className="flex justify-between w-48 mt-5 text-xs text-gray-400">
-      ID : <input type="text"
-        className=" rounded-lg appearance-none border
-     border-black w-500 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2
-      focus:ring-purple-600 focus:border-transparent"/>
-      PW : <input type="text" />
-      <button type="button"
-        className="relative after:absolute after:content-[''] after:w-px after:h-2.5 after:top-1 
-    after:left-32 after:bg-gray-300">아이디/비밀번호 찾기</button>
+  const style = {
+    backgroundColor: 'black',
+    color : 'white',
+  }
 
-{/* ID : <input type="text" onChange={handleUsername}
-        className=" rounded-lg appearance-none border
-     border-black w-500 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2
-      focus:ring-purple-600 focus:border-transparent"/>
-      PW : <input type="text" onChange={handlePassword} />
-      <button type="button"
-        className="relative after:absolute after:content-[''] after:w-px after:h-2.5 after:top-1 
-    after:left-32 after:bg-gray-300">아이디/비밀번호 찾기</button> */}
+  return (<>
+<body style={style}>
+<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img className="mx-auto h-200 w-auto" src="https://cdn-icons-png.flaticon.com/128/4169/4169062.png" alt="Your Company"/>
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight to-blue-500">Sign in to continue</h2>
+  </div>
 
-      <button type="button" ref={"./join"}>회원가입</button>
-    </div>
-  );
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form className="space-y-6" action="#" method="POST">
+      <div>
+        <label htmlFor="email" className="block text-sm font-medium leading-6 text-balance">ID :</label>
+        <div className="mt-2">
+          <input id="username" name="username" type="text" autoComplete="email" required className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-cyan-600 shadow-sm ring-1 ring-inset ring-red-500 focus:ring-2 focus:ring-inset focus:ring-balck sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label htmlFor="password" className="block text-sm font-medium leading-6">Password : </label>
+          <div className="text-sm">
+            <a href="#" className="font-semibold text-red-500 hover:text-red-400">Forgot password?</a>
+          </div>
+        </div>
+        <div className="mt-2">
+          <input id="password" name="password" type="password"  autoComplete="current-password" required className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-red-500 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" className="flex w-full justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">Sign in</button>
+      </div>
+    </form>
+
+    <p className="mt-10 text-center text-sm text-gray-400">
+      Not a member?&nbsp;&nbsp;&nbsp;
+      <a href="#" className="font-semibold leading-6 text-red-500 hover:text-red-400">join us</a>
+    </p>
+  </div>
+</div>
+
+</body>
+  </>);
 }
